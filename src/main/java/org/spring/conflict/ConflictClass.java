3efe0faa707842b3f875967ddd97be738f79b615
Alla <allabreus@gmail.com>
2026-07-22 06:47:40 +0300

    change feature

--- src/main/java/org/spring/conflict/ConflictClass.java
+++ src/main/java/org/spring/conflict/ConflictClass.java
@@ -2,6 +2,6 @@ package org.spring.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("Tis is my conflict class!!!");
+        System.out.println("conflict feature");
     }
 }
