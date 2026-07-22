91fa2b9a86fb38c178b7290c0d5ed53c6cd94581
Alla <allabreus@gmail.com>
2026-07-22 06:46:40 +0300

    changes in master again

--- src/main/java/org/spring/conflict/ConflictClass.java
+++ src/main/java/org/spring/conflict/ConflictClass.java
@@ -2,6 +2,6 @@ package org.spring.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("Changes masters");
+        System.out.println("Changes masters again");
     }
 }
