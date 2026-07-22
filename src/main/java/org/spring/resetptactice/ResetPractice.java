b1189dc0682727c49efe4916a7c5e064a4bc5a48
Alla <allabreus@gmail.com>
2026-07-22 06:41:29 +0300

    commit masage

---
+++ src/main/java/org/spring/resetptactice/ResetPractice.java
@@ -0,0 +1,9 @@
+package org.spring.resetptactice;
+
+public class ResetPractice {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/org/spring/stash/StashDemo.java
@@ -0,0 +1,7 @@
+package org.spring.stash;
+
+public class StashDemo {
+    public static void main(String[] args) {
+        System.out.println("Stash demo");
+    }
+}
