472d2d355c5815f1871e88395c304f0b468513c6
Alla <allabreus@gmail.com>
2026-07-22 06:43:46 +0300

    commit masage

--- src/main/java/org/spring/conflict/ConflictClass.java
+++ src/main/java/org/spring/conflict/ConflictClass.java
@@ -2,6 +2,6 @@ package org.spring.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("Changes masterrs");
+        System.out.println("Changes masters");
     }
 }
